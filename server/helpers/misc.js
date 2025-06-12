c70db7a5d0e412293419a5f33c45473f257f7732
Shreehari Acharya <shreehari.acharya.06@gmail.com>
2025-06-12 16:56:24 +0530

    Add a function to get an array of all the 365/366 dates

---
+++ server/helpers/misc.js
@@ -0,0 +1,9 @@
+export function generateAllDatesInYear(start, end) {
+  const dates = [];
+  const current = new Date(start);
+  while (current <= end) {
+    dates.push(current.toISOString().split('T')[0]);
+    current.setDate(current.getDate() + 1);
+  }
+  return dates;
+}
