c81c5cb38540e35d2862bcc9380500b7232285da
Shreehari Acharya <shreehari.acharya.06@gmail.com>
2025-06-13 23:06:10 +0530

    Add username field to redis

--- server/exports/redis.js
+++ server/exports/redis.js
@@ -7,6 +7,7 @@ const redisClient = new Redis(
         host: process.env.REDIS_HOST || 'localhost',
         port: process.env.REDIS_PORT || 6379,
         password: process.env.REDIS_PASSWORD || undefined,
+        username: process.env.REDIS_USERNAME || undefined,
         db: process.env.REDIS_DB || 0,
         retryStrategy: (times) => {
             // Exponential backoff for reconnection
