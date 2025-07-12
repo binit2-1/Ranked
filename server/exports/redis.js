615c3f690a01ccead41388cc3499876b9da6b8c1
binit <binitgupta.1711@gmail.com>
2025-07-12 16:33:01 +0530

    family fix

--- server/exports/redis.js
+++ server/exports/redis.js
@@ -2,7 +2,7 @@ import Redis from 'ioredis';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const redisClient = new Redis(process.env.REDIS_URL + '?family=0');
+const redisClient = new Redis(process.env.REDIS_URL);
 
 export default redisClient;
 
